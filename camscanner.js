8f751ea19af5791c23998b54fd9c2f80340aecf2
Marcio2536 <61924136+Marcio2536@users.noreply.github.com>
2021-04-10 20:22:38 +0800

    Add files via upload

--- camscanner.js
+++ camscanner.js
@@ -1,3 +1,3 @@
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"3563012200"}}};
+obj = {"data":{"psnl_vip_property":{"svip": 1,"expiry":"3563012200"}}};
 $done({body: JSON.stringify(obj)});
